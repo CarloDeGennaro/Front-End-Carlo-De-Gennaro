ab5618d052f987715f87eb6b6b7fe31720b68600
CarloDeGennaro <carlodegennaro1@gmail.com>
2018-10-23 18:30:36 +0200

    scomparsa cookie_colore like

---
+++ Progetto/src/js/javascriptHello.js
@@ -0,0 +1,56 @@
+
+var nascondi = document.getElementById("spanid"); //al click su span id
+nascondi.addEventListener("click",  function () {
+    document.getElementById("finepagina").style.display='none'; //nascondi cookie
+});
+
+
+
+var like1 = document.getElementById("like1");
+console.log("like1");
+var like2 = document.getElementById("like2");
+var like3 = document.getElementById("like3");
+var like4 = document.getElementById("like4");
+
+like1.addEventListener("click", function (){
+  if (like1.className === "grandespan") {
+      like1.className = "grandespan2";
+      }
+else
+      {
+       like1.className = "grandespan";
+      }
+});
+
+like2.addEventListener("click", function (){
+  if (like2.className === "grandespan") {
+
+      like2.className = "grandespan2";
+      }
+else
+      {
+       like2.className = "grandespan";
+      }
+});
+
+like3.addEventListener("click", function (){
+  if (like3.className === "grandespan") {
+
+      like3.className = "grandespan2";
+      }
+else
+      {
+       like3.className = "grandespan";
+      }
+});
+
+like4.addEventListener("click", function (){
+  if (like4.className === "grandespan") {
+
+      like4.className = "grandespan2";
+      }
+else
+      {
+       like4.className = "grandespan";
+      }
+});
