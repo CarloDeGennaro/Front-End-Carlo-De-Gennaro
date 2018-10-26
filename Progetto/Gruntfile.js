f40ab6311b5dde0dc02d9c1533034b3e8c436065
CarloDeGennaro <carlodegennaro1@gmail.com>
2018-10-26 17:13:32 +0200

    grunt

--- Progetto/Gruntfile.js
+++ Progetto/Gruntfile.js
@@ -1,3 +1,4 @@
+/*
 module.exports = function(grunt)
 {
 grunt.initConfig({
@@ -31,7 +32,8 @@ grunt.initConfig({
   watch: {
       script: {
       files: ['src/js/javascriptHello.js'],
-      tasks: ['browserify']
+      tasks: ['browserify'] //,
+      //  tasks: ['less']
     }
   },
 
@@ -39,7 +41,7 @@ grunt.initConfig({
   cssmin : {
 	target : {
  		src : ['src/css/style2.css'],
-		dest : "dist/css/style_min.css"
+		dest : "dist/css/style_min.min.css"
 	}
 },
 
@@ -47,7 +49,7 @@ grunt.initConfig({
 uglify: {
   target : {
  		src : ['src/js/javascriptHello.js'],
-		dest : "dist/js/js_min.js"
+		dest : "dist/js/js_min.min.js"
 	}
 }
 
@@ -58,10 +60,9 @@ uglify: {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-babel');
-
-
-grunt.loadNpmTasks('grunt-serve');
-grunt.loadNpmTasks('grunt-browserify');//carica il tipo di task
+  grunt.loadNpmTasks('grunt-serve');
+  grunt.loadNpmTasks('grunt-less');
+  grunt.loadNpmTasks('grunt-browserify');//carica il tipo di task
 
 //grunt.task.registerTask('build', ['serve']); //questo è stata creata prima!
 //grunt.task.registerTask('broserify', ['browserify']); // aggiungo un task browserify
@@ -80,4 +81,83 @@ grunt.task.registerTask('build', ['cssmin','serve']); //prova cssmin
 
 //grunt.task.registerTask('build', ['cssmin', 'browserify','babel','uglify','serve']);
 //questo è l'elenco di tutti e tipicamente si mette solo questo
+};
+*/
+
+
+module.exports = function (grunt)
+{
+grunt.initConfig({
+ serve: {
+  options: {
+   port: 9000
+  }
+ },
+ browserify: {
+  dist: {
+   files: {
+    'src/js/boundle.js':'src/js/javascriptHello.js'
+   }
+  }
+ },
+ watch: {
+  script: {
+      files:'src/js/javascriptHello.js',
+      tasks:'browserify'
+          },
+  dist: {
+   files: 'src/css/style2.less',
+   tasks: 'less'
+  }
+ },
+
+ cssmin: {
+  target: {
+   files: {
+    'dist/css/style2.min.css': ['node_modules/bootstrap/dist/css/bootstrap.css','src/css/style2.less']//vettore di 2 elementi
+
+   }
+  }
+ },
+
+ babel: {
+  options: {
+   presets: ['env']
+  },
+  dist: {
+   files: {
+    'src/js/boundle-css.js': 'src/js/boundle.js'
+   }
+  }
+ },
+
+ less: {
+  development: {
+   files: {
+    'src/css/style2.css': 'src/css/style2.less'
+   }
+  }
+ },
+
+ uglify: {
+  target: {
+   files: {
+    'dist/js/boundle.min.js': ['src/js/boundle-css.js', 'node_modules/bootstrap/dist/js/bootstrap.js']
+   }
+  }
+ }
+
+});
+
+grunt.loadNpmTasks('grunt-serve');
+grunt.loadNpmTasks('grunt-browserify');
+grunt.loadNpmTasks('grunt-babel');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+grunt.task.registerTask('develop', ['watch']);
+grunt.task.registerTask('build', ['cssmin','browserify','babel','uglify','serve']);
 };
